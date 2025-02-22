e3c64dcc361ec52108515be6262f2b3de0d6ec79
Mesh-254 <meshack1995@gmail.com>
2025-02-22 11:02:59 +0300

    feat(docker): Configure Docker to work with Redis and Celery
    - Ensured Redis and Celery share the same Docker network
    - Updated  to use the correct Redis hostname
    - Added  to ensure Redis starts before Celery
    - Fixed incorrect port mapping for Redis
    - Restarted and verified connectivity between services

--- alx_travel_app/alx_travel_app/settings.py
+++ alx_travel_app/alx_travel_app/settings.py
@@ -40,8 +40,8 @@ SECRET_KEY = 'django-insecure-r49m@7!zd=13gx-m-d!-ek6#g8!3f__xgxwh)63ijwb2r60u8y
 DEBUG = True
 
 # Celery settings
-CELERY_BROKER_URL = "redis://localhost:6379"
-CELERY_RESULT_BACKEND = "redis://localhost:6379"
+CELERY_BROKER_URL = "redis://redis:6379/0"
+CELERY_RESULT_BACKEND = "redis://redis:6379/0"
 
 
 
